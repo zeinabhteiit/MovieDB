625e78cb6b8a483952f3c109151b3da6b2998828
zeinab <zeinabhteit1@gmail.com>
2024-12-23 12:45:52 +0200

    step 3

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/test', (req, res) => { //TEST
+    res.json({ status: 200, message: 'ok' });
+});
+
+app.get('/time', (req, res) => { //TIME
+    const now = new Date();
+    const time = `${now.getHours()}:${now.getMinutes()} `;
+    res.json({ status: 200, message: time });
+});
